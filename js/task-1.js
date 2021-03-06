bdbb446950fb2421ec70e02fb40d1d3f8925d17c
IvanChernenko <ivanchernenko92@gmail.com>
2021-03-06 14:21:04 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 const ulCategories = document.querySelectorAll('.item');
 console.log(`В списке ${ulCategories.length} категории.`)
 
-const liName = ulCategories.forEach(console.log('Категория:',element => element.querySelector('h2').textContent));
-console.log('Категория:', liName)
+const liName = ulCategories.forEach(element => console.log('Категория:', element.querySelector('h2').textContent,',', `Количество элементов ${element.querySelectorAll('li').length}`));
+
 
