db7043f75a1a0e2594f20d077e83ed7c6665a594
IvanChernenko <ivanchernenko92@gmail.com>
2021-03-09 13:59:01 +0200

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,7 +1,12 @@
-const inputRef = document.querySelector('input');
-
+const inputRef = document.getElementById('validation-input');
+const dataLengthRef = Number(inputRef.getAttribute('data-length'))
 inputRef.addEventListener('blur', onInputBlur);
-
 function onInputBlur() {
-    console.log('qqwwe')
-}
+    if (inputRef.value.length !== dataLengthRef) {
+        inputRef.classList.add('invalid')
+        inputRef.classList.remove('valid')
+    } else {
+        inputRef.classList.add('valid')
+        inputRef.classList.remove('invalid')
+    }
+};
