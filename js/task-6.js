5833a4a1289e987ffae71ffb48460f78b9b15652
IvanChernenko <ivanchernenko92@gmail.com>
2021-03-12 22:02:23 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,12 +1,31 @@
+// const inputRef = document.getElementById('validation-input');
+// const dataLengthRef = Number(inputRef.getAttribute('data-length'))
+// inputRef.addEventListener('blur', onInputBlur);
+// function onInputBlur() {
+//     if (inputRef.value.length !== dataLengthRef) {
+//         inputRef.classList.add('invalid')
+//         inputRef.classList.remove('valid')
+//     } else {
+//         inputRef.classList.add('valid')
+//         inputRef.classList.remove('invalid')
+//     }
+// };
+
 const inputRef = document.getElementById('validation-input');
-const dataLengthRef = Number(inputRef.getAttribute('data-length'))
-inputRef.addEventListener('blur', onInputBlur);
-function onInputBlur() {
-    if (inputRef.value.length !== dataLengthRef) {
-        inputRef.classList.add('invalid')
-        inputRef.classList.remove('valid')
-    } else {
-        inputRef.classList.add('valid')
-        inputRef.classList.remove('invalid')
-    }
+ 
+const validNumber = inputRef.getAttribute('data-length')
+ 
+const updateClass = (addClass, remClass) => {
+    inputRef.classList.remove(`${remClass}`);
+    inputRef.classList.add(`${addClass}`);
 };
+
+const onInputChange = e => {
+    if (e.target.value.length === Number(validNumber)) {
+        updateClass('valid', 'invalid')
+    } else {
+        updateClass('invalid', 'valid')
+    }
+}
+
+inputRef.addEventListener('input', onInputChange);
